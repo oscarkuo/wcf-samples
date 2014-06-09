336de5096dca3925fb0066860fc0c45ba15d7195
Oscar Kuo <oscar@osbek.com>
2014-06-10 10:57:15 +1200

    added the missing file

---
+++ PerRequestLogging/PerRequestLogging.Behaviours/InteractionStateExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+using PerRequestLogging.ValueObjects;
+
+namespace PerRequestLogging.Behaviours
+{
+    public class InteractionStateExtension : IExtension<OperationContext>
+    {
+        public InteractionStateExtension()
+        {
+            State = new Dictionary<string, object>();
+        }
+
+        public IDictionary<string, object> State { get; private set; }
+
+        public void Attach(OperationContext owner) { }
+        public void Detach(OperationContext owner) { }
+    }
+}
